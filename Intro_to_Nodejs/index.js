d46eb645ce62e22235b84f1a1b5ead7addd4f0bb
TirumaliSaiTeja <tsaiteja7@gmail.com>
2020-07-24 18:08:00 +0530

    demo- Node farm

--- Intro_to_Nodejs/index.js
+++ Intro_to_Nodejs/index.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs')
 const http = require('http')
+const url = require('url')
 
 // Synchronous ways
 
@@ -29,9 +30,84 @@ const http = require('http')
 // })
 
 // Creating a simple server
+// Routing in server
+
+// here we use sync beacuse to avoid updating data everything we call
+// for response and with single api call the user can get data
+
+const replaceTemplate = (temp, product) => {
+  // there might be multiple instances so we wrap the productname with
+  // regular expressions with g which means global
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+  output = output.replace(/{%IMAGE%}/g, product.image)
+  output = output.replace(/{%PRICE%}/g, product.price)
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+  output = output.replace(/{%QUANTITY%}/g, product.quantity)
+  output = output.replace(/{%DESCRIPTION%}/g, product.description)
+  output = output.replace(/{%FROM%}/g, product.from)
+  output = output.replace(/{%ID%}/g, product.id)
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+
+  return output
+}
+
+const tempOverview = fs.readFileSync(
+  './templates/template-overview.html',
+  'utf-8',
+)
+const tempCard = fs.readFileSync('./templates/template-cards.html', 'utf-8')
+const tempProduct = fs.readFileSync(
+  './templates/template-product.html',
+  'utf-8',
+)
+
+const data = fs.readFileSync('./dev-data/data.json', 'utf-8')
+const dataObj = JSON.parse(data)
 
 const server = http.createServer((req, res) => {
-  res.end('Hello Welcome to the server')
+  const { query, pathname } = url.parse(req.url, true)
+
+  // Overview
+
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    })
+
+    // Map expects a callback function, which arguments an element in the current loop
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('')
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+    res.end(output)
+
+    // Product
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' })
+    const product = dataObj[query.id]
+    const output = replaceTemplate(tempProduct, product)
+
+    res.end(output)
+
+    // API
+  } else if (pathname === '/API') {
+    res.writeHead(200, {
+      'Content-type': 'application/json',
+    })
+    res.end(data)
+  }
+
+  // Not Found
+  else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'Welcome',
+    })
+    res.end('<h1>Page not found</h1>')
+  }
 })
 
 server.listen(3000, () => {
