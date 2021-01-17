eeb25459726315530b3945ea2b458137bc33c548
TirumaliSaiTeja <tsaiteja7@gmail.com>
2021-01-17 23:42:47 +0530

    adding api key for mailchimp

--- api/NewsLetter_project/app.js
+++ api/NewsLetter_project/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const https = require('https')
 
 const app = express();
 
@@ -17,14 +18,55 @@ app.get('/', function(req,res){
     res.sendFile(__dirname + '/signup.html');
 })
 
-app.post('/', function(req,res){
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
 
-    console.log(firstName, lastName, email);
-})
+
+app.post('/', function(req,res){
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    // structuring the data object with required details
+
+    const data = {
+        members:[
+            {
+                email_address:email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us7.api.mailchimp.com/3.0/lists/1dcaf21da4";
+
+    const options = {
+        method: "POST",
+        auth: "saiteja:9bce763d340a8b0623c4d43e87c34ab3-us7"
+
+    }
+
+    const request = https.request(url, options, function(response){
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    });
+
+    request.write(jsonData);
+    request.end();
+
+});
 
 app.listen(3000, function(){
     console.log('server running');
 })
+
+
+
+// 9bce763d340a8b0623c4d43e87c34ab3-us7
+
+// 1dcaf21da4
